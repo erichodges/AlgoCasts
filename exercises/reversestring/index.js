ac165f9998d3de3877ac03ccdfa75064b364de76
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-11-11 12:48:30 -0800

    initial commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
 module.exports = reverse;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3 to wow interviewer (lol)
+// return str.split('').reduce((rev, char) => char + rev, '');  //shortened version
+
+// longer version
+//   return str.split('').reduce((reversed, character) => {
+//   return character + reversed;
+//   }, '');
+
+
+// 2
+// let reversed = '';
+  
+// for (let character of str) {
+//   reversed = character + reversed;
+// }
+// return reversed;
+
+
+// 1
+// return str.split('').reverse().join(''); 
