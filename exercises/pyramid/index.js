c0f4ed3d7867a6507b64a545cf0c0bb23c7ef231
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-11-15 20:49:15 -0800

    added vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,36 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
-
+function pyramid(n) {
+  
 module.exports = pyramid;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const midpoint = Math.floor((2 * n - 1) / 2);  // n -1 also seems to work.
+
+  // for (let row = 0; row < n; row++) {
+  //   let level = '';
+
+  //   for (let column = 0; column < 2 * n - 1; column++) {
+  //     if (midpoint - row <= column && midpoint + row >= column) {
+  //       level += '#';
+  //     } else {
+  //       level += ' ';
+  //     }
+  //   }
+  //   console.log(level);
+  // }
+}
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
 
+}
 module.exports = vowels;
+
+
+
+
+
+// The recursive solution takes into account that if there is no match the return is null, which is falsy.
+
+// const matches = str.match(/[aeiou]/gi);
+// return matches ? matches.length : 0;
+
+
+
+// With the iterative solution one can discuss a case where an additional string, such as 'abcd' could be included in the array but could not be included in a string used to check for items
+
+// let count = 0;
+// const checker = ['a', 'e', 'i', 'o', 'u'];
+
+// for (let char of str.toLowerCase()) {
+//   if (checker.includes(char)) {
+//     count++;
+//   }
+// }
+// return count;
